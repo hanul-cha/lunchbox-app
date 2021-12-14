d02f0f69ca84a65e9b68954c3da8be4b875ae4e1
hanul-cha <ckgks3014@naver.com>
2021-12-14 16:27:47 +0900

    sql작성 예시

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import "./index.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
