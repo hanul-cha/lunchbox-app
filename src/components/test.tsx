f84eda556f6c392022ce6082c9ff5ac9cfacee61
hanul-cha <ckgks3014@naver.com>
2022-01-19 21:11:40 +0900

    test commit

---
+++ src/components/test.tsx
@@ -0,0 +1,14 @@
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footerContainer">
+        <div>(주)한울도시락</div>
+        <div>ALL right reserved</div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
